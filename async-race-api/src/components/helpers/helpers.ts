dc352f01a5de62cbcc9091c02dffd2c72a433ed9
Anneli Savchenko <foranneli@gmail.com>
2023-01-20 13:08:55 +0100

    feat: add function for getting input value in helpers.ts

--- async-race-api/src/components/helpers/helpers.ts
+++ async-race-api/src/components/helpers/helpers.ts
@@ -1,3 +1,8 @@
 export const generateColor = (): string => {
     return '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
 };
+
+export const getInputValue = (e: Event): string => {
+    const input = e.target as HTMLInputElement;
+    return input.value;
+};
